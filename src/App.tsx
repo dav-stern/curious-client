baf1263d1a25e2341100ac34d507f887ab7e9d54
Florian Jost <florian.jost89@gmail.com>
2019-08-12 11:52:42 +0200

    chore: establish apollo client

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,17 @@
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from '@apollo/react-hooks';
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+const client = new ApolloClient({
+  uri: process.env.SERVER_URL,
+});
+
 const App: React.FC = () => (
-  <div className="App">
-    <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-      <p>
-        Edit
-        <code>
-          src/App.tsx
-        </code>
-        and save to reload.
-      </p>
-      <a
-        className="App-link"
-        href="https://reactjs.org"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Learn React
-      </a>
-    </header>
-  </div>
+  <ApolloProvider client={client}>
+    <div />
+  </ApolloProvider>
 );
+
 
 export default App;
