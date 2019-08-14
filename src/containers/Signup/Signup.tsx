c343bfc6a6a8b2329d8cf32478fffceaec1837bc
Fábio <videiraft@gmail.com>
2019-08-14 21:42:59 +0200

    refactor: refactor login and signup containers

--- src/containers/Signup/Signup.tsx
+++ src/containers/Signup/Signup.tsx
@@ -13,54 +13,44 @@ const SIGN_UP = gql`
 // interface token {
 //   signup: string
 // }
-
 // interface dataSignup {
 //   data: token
 // }
 
 const Signup: React.FC = () => {
   const client = useApolloClient();
-  const [nameInput, setNameInput] = useState('');
-  const [passwordInput, setPasswordInput] = useState('');
-  const [emailInput, setEmailInput] = useState('');
+  const [inputs, setInputs] = useState({ name: '', email: '', password: '' });
   const [errorMsg, setErrorMsg] = useState('');
-
   const [signup] = useMutation(SIGN_UP, {
-    variables: { name: nameInput, email: emailInput, password: passwordInput },
+    variables: { name: inputs.name, email: inputs.email, password: inputs.password },
   });
 
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
+    // TODO: any??
     const res: any = await signup();
     if (res.data.signup) {
       localStorage.setItem('token', res.data.signup);
       const { id, name, email } = jwtDecode(res.data.signup);
       client.writeData({ data: { id, name, email } });
-      setNameInput('');
-      setPasswordInput('');
-      setEmailInput('');
+      setInputs({ name: '', email: '', password: '' });
+      setErrorMsg('');
     } else {
       setErrorMsg('This email already exists!');
     }
   };
 
+  // TODO: abstract handleChange on the component
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { value, name } = e.target;
-
-    if (name === 'name') {
-      setNameInput(value);
-    } else if (name === 'email') {
-      setEmailInput(value);
-    } else if (name === 'password') {
-      setPasswordInput(value);
-    }
+    setInputs({ ...inputs, [name]: value });
   };
 
   return (
     <AuthForm
-      name={nameInput}
-      email={emailInput}
-      password={passwordInput}
+      name={inputs.name}
+      email={inputs.email}
+      password={inputs.password}
       handleSubmit={handleSubmit}
       handleChange={handleChange}
       errorMsg={errorMsg}
