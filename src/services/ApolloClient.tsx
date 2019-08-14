3dfd6af5075eb48b6860f58363e1d5d793691057
Fábio <videiraft@gmail.com>
2019-08-14 09:55:52 +0200

    chore: install apollo cache and set authorization service work todo

--- src/services/ApolloClient.tsx
+++ src/services/ApolloClient.tsx
@@ -1,7 +1,20 @@
 import ApolloClient from 'apollo-boost';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+
+const cache = new InMemoryCache();
 
 const client = new ApolloClient({
+  cache,
   uri: process.env.REACT_APP_SERVER_URL,
+});
+
+cache.writeData({
+  data: {
+    id: '',
+    user: '',
+    email: '',
+    roadmaps: [],
+  },
 });
 
 export default client;
