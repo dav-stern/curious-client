fefb66f955341e34d403924af0cd01ec2bb749d8
Fábio <videiraft@gmail.com>
2019-08-15 10:14:39 +0200

    feat: set headers authorization

--- src/services/ApolloClient.tsx
+++ src/services/ApolloClient.tsx
@@ -1,11 +1,29 @@
-import ApolloClient from 'apollo-boost';
+import ApolloClient from 'apollo-client';
+import { createHttpLink } from 'apollo-link-http';
+import { setContext } from 'apollo-link-context';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 
 const cache = new InMemoryCache();
 
+const httpLink = createHttpLink({
+  uri: process.env.REACT_APP_SERVER_URL,
+});
+
+const authLink = setContext((_, { headers }) => {
+  // get the authentication token from local storage if it exists
+  const token = localStorage.getItem('token');
+  // return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
 const client = new ApolloClient({
   cache,
-  uri: process.env.REACT_APP_SERVER_URL,
+  link: authLink.concat(httpLink),
 });
 
 cache.writeData({
