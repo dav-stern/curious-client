251161740a7dac20020816ebddc188ce5d63d4ea
Florian Jost <florian.jost89@gmail.com>
2019-08-16 14:48:44 +0200

    feat: route to discover and add navbar on discover container

--- src/containers/Discover/Discover.tsx
+++ src/containers/Discover/Discover.tsx
@@ -1,1 +1,17 @@
-export {};
+import React from 'react';
+import './Discover.css';
+import { useApolloClient } from '@apollo/react-hooks';
+import Navbar from '../../components/Navbar/Navbar';
+
+const Discover: React.FC = () => {
+  const client = useApolloClient();
+  console.log(client); // eslint-disable-line
+  return (
+    <>
+      <Navbar />
+    </>
+  );
+};
+
+
+export default Discover;
