8bdf5d06e5adceb5eb511e1b1c0415609d7a8d1b
Andre Coutinho <andrelelliscoutinho@gmail.com>
2019-08-19 16:19:00 +0200

    feat: implement initial version of the TopicDetails container

--- src/components/TopicDetails/TopicDetails.tsx
+++ src/components/TopicDetails/TopicDetails.tsx
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import gql from 'graphql-tag';
-import { useQuery } from '@apollo/react-hooks';
+import { useQuery, useMutation } from '@apollo/react-hooks';
+
+import './TopicDetails.css';
 
 interface ITopicDetailsProps {
   topicId: number
@@ -24,44 +26,74 @@ const GET_TOPIC_DETAILS = gql`
   }
 `;
 
+const UPDATE_TOPIC = gql`
+  mutation updateTopic($id: ID! $title: String, $description: String $resources: String $rowNumber: Int) {
+    updateTopic(id: $id title: $title, description: $description, resources: $resources, rowNumber: $rowNumber) {
+      id
+      title
+      description
+      resources
+      rowNumber
+    }
+  }
+`;
+
 // create onClick function that toggles CSS displays for all fields
 // create function to use mutation for the save button
 
 const TopicDetails : React.FC<ITopicDetailsProps> = ({ topicId }) => {
-  const { data, loading, error } = useQuery(GET_TOPIC_DETAILS, {
+  const [titleInput, setTitleInput] = useState('');
+  const [descriptionInput, setDescriptionInput] = useState('');
+  const [resourcesInput, setResourcesInput] = useState('');
+
+  const [updateTopic] = useMutation(UPDATE_TOPIC, {
+    variables: {
+      id: topicId,
+      title: titleInput,
+      description: descriptionInput,
+      resources: resourcesInput,
+    },
+  });
+
+  const { data, loading, refetch } = useQuery(GET_TOPIC_DETAILS, {
     variables: { id: topicId },
   });
   if (loading) return <p>Loading...</p>;
-  if (error) return <p>Error :(</p>;
   if (!data.topics[0]) return null;
 
   // add below -> , completed, Checklist
   const { title, description, resources } = data.topics[0];
+
+  const handleChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    const inputClass = e.target.className;
+    if (inputClass === 'topic-title') setTitleInput(e.target.value);
+    if (inputClass === 'topic-description') setDescriptionInput(e.target.value);
+    if (inputClass === 'topic-resources') setResourcesInput(e.target.value);
+  };
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    await updateTopic();
+    refetch();
+  };
+
   return (
     <div className="topic-details-card">
-      <div className="topic-title-block">
-        <h3 className="toipc-title">{title}</h3>
-        <input className="topic-title-draft" />
-      </div>
-
-      <div className="topic-description-block">
-        <h4>Description</h4>
-        <div className="topic-description">
-          <p>{description}</p>
+      <form onSubmit={handleSubmit}>
+        <div className="title-block">
+          <textarea className="topic-title" value={titleInput} onChange={handleChange} />
         </div>
-        <textarea className="topic-description-draft" />
-      </div>
-
-      <div className="topic-resources-block">
-        <h3>Resources</h3>
-        <div className="topic-resources">
-          {resources}
+        <div className="description-block">
+          <h4>Description:</h4>
+          <textarea className="topic-description" value={descriptionInput} onChange={handleChange} />
         </div>
-        <textarea className="topic-resources-draft" />
-      </div>
-
+        <div className="resources-block">
+          <h4>Resources:</h4>
+          <textarea className="topic-resources" value={resourcesInput} onChange={handleChange} />
+        </div>
+        <button type="submit">Save</button>
+      </form>
       {/* <Checklist /> */}
-      <button type="submit">Save</button>
     </div>
   );
 };
